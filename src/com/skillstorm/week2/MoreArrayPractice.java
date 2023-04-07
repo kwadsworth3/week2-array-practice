2f755c71663869c1ffda01a0a00245f2ddab3efd
kwadsworth3 <128946762+kwadsworth3@users.noreply.github.com>
2023-04-07 11:39:43 -0500

    Practice with Loops and Arrays

--- src/com/skillstorm/week2/MoreArrayPractice.java
+++ src/com/skillstorm/week2/MoreArrayPractice.java
@@ -8,7 +8,7 @@ public class MoreArrayPractice {
 		int[] c = { 2 };
 		int[] d = {};
 		int[] e = null;
-		int[] f = {-2, 0, 18, -28};
+		int[] f = { -2, 0, 18, -28 };
 		System.out.println(findMax(a));
 		System.out.println(findMax(b));
 		System.out.println(findMax(c));
@@ -16,6 +16,13 @@ public class MoreArrayPractice {
 		System.out.println(findMax(e));
 		System.out.println(findMax(f));
 		System.out.println("-----Part Two-----");
+		System.out.println(findMaxImproved(a));
+		System.out.println(findMaxImproved(b));
+		System.out.println(findMaxImproved(c));
+		System.out.println(findMaxImproved(d));
+		System.out.println(findMaxImproved(e));
+		System.out.println(findMaxImproved(f));
+		System.out.println("-----Part Three-----");
 		System.out.println(findMin(a));
 		System.out.println(findMin(b));
 		System.out.println(findMin(c));
@@ -39,12 +46,26 @@ public class MoreArrayPractice {
 			return trackerMax;
 		}
 	}
-	
+
+	public static int findMaxImproved(int[] numbersSomeMore) {
+		int index = 0;
+		int trackerMaxIm = Integer.MIN_VALUE;
+		if (numbersSomeMore == null || numbersSomeMore.length <= 0) {
+			return trackerMaxIm; // returns -2147483648 instead of 0 if used index
+		} else {
+			while (index < numbersSomeMore.length) {
+				trackerMaxIm = Math.max(trackerMaxIm, numbersSomeMore[index]);
+				index++;
+			}
+			return trackerMaxIm;
+		}
+	}
+
 	public static int findMin(int[] numbersAgain) {
 		int index = 0;
 		int trackerMin = Integer.MAX_VALUE;
 		if (numbersAgain == null || numbersAgain.length <= 0) {
-			return trackerMin; // returns 2147483647 instead of 0
+			return trackerMin; // returns 2147483647 instead of 0 if used index
 		} else {
 			while (index < numbersAgain.length) {
 				trackerMin = Math.min(trackerMin, numbersAgain[index]);
